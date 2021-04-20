37c26e0841b3dd3567b157efec64c7e6514f5a89
varunon9 <varunon9@gmail.com>
2021-04-20 19:08:08 +0530

    covidHelpEsFetch function: changed response structure to meet API gateway expectation

--- lambdas/python/covidHelpEsFetch/lambda_function.py
+++ lambdas/python/covidHelpEsFetch/lambda_function.py
@@ -6,8 +6,16 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch('https://vpc-covid-help-ggo4aqwgkblo7uh2oxp4izirjy.ap-south-1.es.amazonaws.com/')
 
 def lambda_handler(event, context):
+    index = event["queryStringParameters"]["index"]
+    print(index)
     try:
-        res = es.search(index=event["index"], body={"query": {"match_all": {}}})
-        return res.get("hits", {}).get("hits", [])
+        res = es.search(index=index, body={"query": {"match_all": {}}})
+        parsed_res = res.get("hits", {}).get("hits", [])
+        print(len(parsed_res))
+        return {
+            "statusCode": 200,
+            "body": json.dumps(parsed_res)
+        }
     except Exception as e:
+        print(e)
         return "Error in fetch: {}".format(e)
