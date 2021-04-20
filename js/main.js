d7f5ac2ab2fd6dec1d43190ed48d8ff4510a4304
varunon9 <varunon9@gmail.com>
2021-04-20 23:02:17 +0530

    Enabled CORS in Lambda APIs, integrated GET Oxygen data API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,53 @@
-// https://datatables.net/examples/styling/semanticui
+// https://datatables.net/manual/ajax
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#oxygenTable').DataTable({
+    ajax: {
+      url: 'https://4tomrkuta3.execute-api.ap-south-1.amazonaws.com/dev/?index=oxygen',
+      dataSrc: ''
+    },
+    columns: [
+      {
+        data: '_source.vendor_name'
+      },
+      {
+        data: '_source.person_name'
+      },
+      {
+        data: '_source.contact'
+      },
+      {
+        data: '_source.state'
+      },
+      {
+        data: '_source.city'
+      },
+      {
+        data: '_source.quantity'
+      },
+      {
+        data: '_source.price'
+      },
+      {
+        data: '_source.link'
+      },
+      {
+        data: '_source.comments'
+      },
+      /*{
+        data: '_source.last_updated',
+        render: function(data) {
+          return new Date(data).toLocaleDateString()
+        }
+      },*/
+      {
+        data: '_source.created_at',
+        render: function(data) {
+          return new Date(data).toLocaleDateString()
+        }
+      },
+      {
+        data: '_source.verified'
+      }
+    ]
+  });
 } );
