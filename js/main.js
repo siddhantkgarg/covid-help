673fca799a1e79e9ac9f8c2c3e802c772516f160
varunon9 <varunon9@gmail.com>
2021-04-21 00:03:29 +0530

    integrated GET hospital_beds & medicines API

--- js/main.js
+++ js/main.js
@@ -1,53 +1,161 @@
 // https://datatables.net/manual/ajax
 $(document).ready(function() {
-  $('#oxygenTable').DataTable({
-    ajax: {
-      url: 'https://4tomrkuta3.execute-api.ap-south-1.amazonaws.com/dev/?index=oxygen',
-      dataSrc: ''
-    },
-    columns: [
-      {
-        data: '_source.vendor_name'
+  try {
+    $('#hospitalBedsTable').DataTable({
+      ajax: {
+        url: 'https://4tomrkuta3.execute-api.ap-south-1.amazonaws.com/dev/?index=hospital_beds',
+        dataSrc: ''
       },
-      {
-        data: '_source.person_name'
-      },
-      {
-        data: '_source.contact'
-      },
-      {
-        data: '_source.state'
-      },
-      {
-        data: '_source.city'
-      },
-      {
-        data: '_source.quantity'
-      },
-      {
-        data: '_source.price'
-      },
-      {
-        data: '_source.link'
-      },
-      {
-        data: '_source.comments'
-      },
-      /*{
-        data: '_source.last_updated',
-        render: function(data) {
-          return new Date(data).toLocaleDateString()
+      columns: [
+        {
+          data: '_source.hospital_name'
+        },
+        {
+          data: '_source.state'
+        },
+        {
+          data: '_source.city'
+        },
+        {
+          data: '_source.beds_count'
+        },
+        {
+          data: '_source.icu_beds_count'
+        },
+        {
+          data: '_source.link'
+        },
+        {
+          data: '_source.comments'
+        },
+        /*{
+          data: '_source.last_updated',
+          render: function(data) {
+            return new Date(data).toLocaleDateString()
+          }
+        },*/
+        {
+          data: '_source.created_at',
+          render: function(data) {
+            return new Date(data).toLocaleDateString()
+          }
+        },
+        {
+          data: '_source.verified'
         }
-      },*/
-      {
-        data: '_source.created_at',
-        render: function(data) {
-          return new Date(data).toLocaleDateString()
-        }
+      ]
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    $('#medicinesTable').DataTable({
+      ajax: {
+        url: 'https://4tomrkuta3.execute-api.ap-south-1.amazonaws.com/dev/?index=medicines',
+        dataSrc: ''
       },
-      {
-        data: '_source.verified'
-      }
-    ]
-  });
+      columns: [
+        {
+          data: '_source.person_name'
+        },
+        {
+          data: '_source.contact'
+        },
+        {
+          data: '_source.medicine_name'
+        },
+        {
+          data: '_source.state'
+        },
+        {
+          data: '_source.city'
+        },
+        {
+          data: '_source.quantity'
+        },
+        {
+          data: '_source.price'
+        },
+        {
+          data: '_source.link'
+        },
+        {
+          data: '_source.comments'
+        },
+        /*{
+          data: '_source.last_updated',
+          render: function(data) {
+            return new Date(data).toLocaleDateString()
+          }
+        },*/
+        {
+          data: '_source.created_at',
+          render: function(data) {
+            return new Date(data).toLocaleDateString()
+          }
+        },
+        {
+          data: '_source.verified'
+        }
+      ]
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    $('#oxygenTable').DataTable({
+      ajax: {
+        url: 'https://4tomrkuta3.execute-api.ap-south-1.amazonaws.com/dev/?index=oxygen',
+        dataSrc: ''
+      },
+      columns: [
+        {
+          data: '_source.vendor_name'
+        },
+        {
+          data: '_source.person_name'
+        },
+        {
+          data: '_source.contact'
+        },
+        {
+          data: '_source.state'
+        },
+        {
+          data: '_source.city'
+        },
+        {
+          data: '_source.quantity'
+        },
+        {
+          data: '_source.price'
+        },
+        {
+          data: '_source.link'
+        },
+        {
+          data: '_source.comments'
+        },
+        /*{
+          data: '_source.last_updated',
+          render: function(data) {
+            return new Date(data).toLocaleDateString()
+          }
+        },*/
+        {
+          data: '_source.created_at',
+          render: function(data) {
+            return new Date(data).toLocaleDateString()
+          }
+        },
+        {
+          data: '_source.verified'
+        }
+      ]
+    });
+  } catch (error) {
+    console.error(error);
+  }
 } );
