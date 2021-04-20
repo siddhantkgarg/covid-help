70b5a281f8fcc4c88d0d0f152920c898cc3fc60d
codingHobbit <49631773+codingHobbit@users.noreply.github.com>
2021-04-20 19:53:54 +0530

    Update lambda_function.py

--- lambdas/python/covidHelpEsUpsert/lambda_function.py
+++ lambdas/python/covidHelpEsUpsert/lambda_function.py
@@ -6,17 +6,29 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch('https://vpc-covid-help-ggo4aqwgkblo7uh2oxp4izirjy.ap-south-1.es.amazonaws.com/')
 
 def lambda_handler(event, context):
+    index = event["queryStringParameters"]["index"]
+    print(index)
     try:
-        doc = event['data']
-        if 'id' in event['data'] and event['data']['id']:
-            if event['token'] == 'replace_dummy_token':
-                res = es.update(index=event['index'], id=event['data']['id'], body={'doc': doc})
-                return res['result']
+        doc = event['body']['data']
+        if 'id' in event['body']['data'] and event['body']['data']['id']:
+            if event['body']['token'] == 'replace_dummy_token':
+                res = es.update(index=index, id=event['body']['data']['id'], body={'doc': doc})
+                return {
+                    "statusCode": 200,
+                    "body": json.dumps({"status": res['result']})
+                }
             else:
-                return "Authorization failed"
+                return {
+                    "statusCode": 400,
+                    "body": json.dumps({"status": "Authorization failed"})
+                }
         else:
             doc['verified'] = False
-            res = es.index(index=event['index'], body=doc)
-            return res['result']
+            res = es.index(index=index, body=doc)
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"status": res['result']})
+            }
     except Exception as e:
+        print(e)
         return "Error in upsert: {}".format(e)
